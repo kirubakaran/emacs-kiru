77ed70b11b418a8fa07c335e11cfbfa90e760dd5
Kiru <kiru@kiru-dev.(none)>
2011-04-02 12:43:59 -0700

    follow vc controlled symlinks without confirmation
    http://groups.google.com/group/comp.emacs/browse_thread/thread/8a26092f4e0cee50/eeb2aee0e42ffdc0

--- dot-emacs.el
+++ dot-emacs.el
@@ -117,7 +117,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil))
+ '(blink-cursor-mode nil)
+ '(vc-follow-symlinks nil))
 
 (defun alt-colors-2 ()
   (progn
@@ -854,3 +855,10 @@
 (require 'erlang-start)
 (require 'distel)
 (distel-setup)
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
