d8e11055b78daf9e9e2bf8bbb747132100c29d29
KA <git@kirubakaran.com>
2014-06-16 10:20:10 -0700

    Try magit fullscreen again

--- dot-emacs.el
+++ dot-emacs.el
@@ -1111,20 +1111,20 @@
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
 
-;; ;; full screen magit-status
+;; full screen magit-status
 
-;; (defadvice magit-status (around magit-fullscreen activate)
-;;   (window-configuration-to-register :magit-fullscreen)
-;;   ad-do-it
-;;   (delete-other-windows))
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
 
-;; (defun magit-quit-session ()
-;;   "Restores the previous window configuration and kills the magit buffer"
-;;   (interactive)
-;;   (kill-buffer)
-;;   (jump-to-register :magit-fullscreen))
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
 
-;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
