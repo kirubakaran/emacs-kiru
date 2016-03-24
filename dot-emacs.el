c4a1ee90dfe21272535a774675f6718583dd600a
Kirubakaran Athmanathan <emacs@kirubakaran.com>
2016-03-24 16:38:49 -0700

    react

--- dot-emacs.el
+++ dot-emacs.el
@@ -924,6 +924,9 @@
       ;; Add the original Emacs Lisp Package Archive
       (add-to-list 'package-archives
                    '("elpa" . "http://tromey.com/elpa/"))
+      (add-to-list 'package-archives
+                   '("gnu" . "http://elpa.gnu.org/packages/"))
+
       ;; Add the user-contributed repository
       ;; (add-to-list 'package-archives
       ;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -1351,3 +1354,58 @@
 (setq gc-cons-threshold 20000000)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; for react ---------------------------------------------------------
+;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-quoting t))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
+
+;; -------------------------------------------------------------------
